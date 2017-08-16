fc001afe340fd66de2b184149451c224351be979
Disgusting Wall <disgusting.wall@gmail.com>
2017-08-16 12:20:29 -0400

    Make website usable from local filesystem by making file references relative to the current directory and using GET variables in URL rather than slashes

--- public/js/player.js
+++ public/js/player.js
@@ -1,5 +1,23 @@
 (function() {
-	var hash = location.pathname.split('/').pop();
+	//Get the GET variables out of the URL
+	var $_GET = {};
+	if(document.location.toString().indexOf('?') !== -1) {
+		var query = document.location
+					.toString()
+					// get the query string
+					.replace(/^.*?\?/, '')
+					// and remove any existing hash string (thanks, @vrijdenker)
+					.replace(/#.*$/, '')
+					.split('&');
+		
+		for(var i=0, l=query.length; i<l; i++) {
+		   var aux = decodeURIComponent(query[i]).split('=');
+		   $_GET[aux[0]] = aux[1];
+		}
+	}
+	
+	//Set the hash of the video to "v" in the GET variables
+	var hash = $_GET['v'];
 	// hash = 'QmU1GSqu4w29Pt7EEM57Lhte8Lce6e7kuhRHo6rSNb2UaC';
 	var playerHolder = $('#player__holder');
 	var player = $('#player');
